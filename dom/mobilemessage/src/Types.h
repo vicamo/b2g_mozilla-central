vishalgautam/b2g_releases-mozilla-central

Bug 805733 - Part 2/5: dom implementation

dom/sms/src/Types.h
@@ -46,7 +46,8 @@ enum ReadState {
 
 // For SmsFilterData.messageClass
 enum MessageClass {
-  eMessageClass_Normal = 0,
+  eMessageClass_Unknown = -1,
+  eMessageClass_Normal,
   eMessageClass_Class0,
   eMessageClass_Class1,
   eMessageClass_Class2,
@@ -97,7 +98,7 @@ struct ParamTraits<mozilla::dom::sms::ReadState>
 template <>
 struct ParamTraits<mozilla::dom::sms::MessageClass>
   : public EnumSerializer<mozilla::dom::sms::MessageClass,
-                          mozilla::dom::sms::eMessageClass_Normal,
+                          mozilla::dom::sms::eMessageClass_Unknown,
                           mozilla::dom::sms::eMessageClass_EndGuard>
 {};
 

vicamo/b2g_mozilla-central

dom/mobilemessage/src/Types.h
@@ -49,7 +49,8 @@ enum ReadState {
 
 // For {Mms,Sms}FilterData.messageClass.
 enum MessageClass {
-  eMessageClass_Normal = 0,
+  eMessageClass_Unknown = -1,
+  eMessageClass_Normal,
   eMessageClass_Class0,
   eMessageClass_Class1,
   eMessageClass_Class2,
@@ -108,7 +109,7 @@ struct ParamTraits<mozilla::dom::mobilemessage::ReadState>
 template <>
 struct ParamTraits<mozilla::dom::mobilemessage::MessageClass>
   : public EnumSerializer<mozilla::dom::mobilemessage::MessageClass,
-                          mozilla::dom::mobilemessage::eMessageClass_Normal,
+                          mozilla::dom::mobilemessage::eMessageClass_Unknown,
                           mozilla::dom::mobilemessage::eMessageClass_EndGuard>
 {};
 