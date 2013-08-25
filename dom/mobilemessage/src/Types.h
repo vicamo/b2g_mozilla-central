vishalgautam/b2g_releases-mozilla-central

Bug 805378 - Part 2/5: DOM implementation

dom/sms/src/Types.h
@@ -27,7 +27,8 @@ enum DeliveryState {
 
 // For SmsMessageData.deliveryStatus.
 enum DeliveryStatus {
-  eDeliveryStatus_NotApplicable = 0,
+  eDeliveryStatus_Unknown = -1,
+  eDeliveryStatus_NotApplicable,
   eDeliveryStatus_Success,
   eDeliveryStatus_Pending,
   eDeliveryStatus_Error,
@@ -77,7 +78,7 @@ struct ParamTraits<mozilla::dom::sms::DeliveryState>
 template <>
 struct ParamTraits<mozilla::dom::sms::DeliveryStatus>
   : public EnumSerializer<mozilla::dom::sms::DeliveryStatus,
-                          mozilla::dom::sms::eDeliveryStatus_NotApplicable,
+                          mozilla::dom::sms::eDeliveryStatus_Unknown,
                           mozilla::dom::sms::eDeliveryStatus_EndGuard>
 {};
 

vicamo/b2g_mozilla-central

dom/mobilemessage/src/Types.h
@@ -28,7 +28,8 @@ enum DeliveryState {
 
 // For {Mms,Sms}MessageData.deliveryStatus.
 enum DeliveryStatus {
-  eDeliveryStatus_NotApplicable = 0,
+  eDeliveryStatus_Unknown = -1,
+  eDeliveryStatus_NotApplicable,
   eDeliveryStatus_Success,
   eDeliveryStatus_Pending,
   eDeliveryStatus_Error,
@@ -88,7 +89,7 @@ struct ParamTraits<mozilla::dom::mobilemessage::DeliveryState>
 template <>
 struct ParamTraits<mozilla::dom::mobilemessage::DeliveryStatus>
   : public EnumSerializer<mozilla::dom::mobilemessage::DeliveryStatus,
-                          mozilla::dom::mobilemessage::eDeliveryStatus_NotApplicable,
+                          mozilla::dom::mobilemessage::eDeliveryStatus_Unknown,
                           mozilla::dom::mobilemessage::eDeliveryStatus_EndGuard>
 {};
 