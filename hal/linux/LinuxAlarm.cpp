4b0db7591b2a65ce93a80bfd858dbc1b1a0a56e3
You-Sheng Yang <vicamo@gmail.com>
2014-05-08 02:56:26 +0800

    Bug 866657 - 3/N: add empty hal/linux/LinuxAlarm.cpp.

---
+++ hal/linux/LinuxAlarm.cpp
@@ -0,0 +1,30 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set sw=2 ts=8 et ft=cpp : */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/Hal.h"
+
+namespace mozilla {
+namespace hal_impl {
+
+bool
+EnableAlarm()
+{
+  return false;
+}
+
+void
+DisableAlarm()
+{
+}
+
+bool
+SetAlarm(int32_t aSeconds, int32_t aNanoseconds)
+{
+  return false;
+}
+
+} // hal_impl
+} // namespace mozilla
