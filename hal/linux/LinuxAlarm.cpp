1ecb78c1182cecffa993e1e43ba24e22f7d61b38
You-Sheng Yang <vicamo@gmail.com>
2014-05-08 02:56:26 +0800

    Bug 866657 - 4/N: complete Linux alarm implementation.

--- hal/linux/LinuxAlarm.cpp
+++ hal/linux/LinuxAlarm.cpp
@@ -4,26 +4,50 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+
 #include "mozilla/Hal.h"
 
 namespace mozilla {
 namespace hal_impl {
 
+namespace {
+
+int sRtcFd = -1;
+
+} // anonymous namespace
+
 bool
 EnableAlarm()
 {
-  return false;
+  MOZ_ASSERT(sRtcFd < 0);
+
+  sRtcFd = open("/dev/rtc0", O_RDONLY);
+  if (sRtcFd < 0) {
+    HAL_LOG(("Failed to open rtc device: %s.", strerror(errno)));
+    return false;
+  }
+
+  return true;
 }
 
 void
 DisableAlarm()
 {
+  MOZ_ASSERT(sRtcFd >= 0);
 }
 
 bool
 SetAlarm(int32_t aSeconds, int32_t aNanoseconds)
 {
-  return false;
+  if (sRtcFd < 0) {
+    HAL_LOG(("We should have enabled the alarm."));
+    return false;
+  }
+
+  return true;
 }
 
 } // hal_impl
